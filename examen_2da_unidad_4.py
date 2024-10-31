1b339b26c0bbc4668b241060c8ef781e811cfc1b
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-31 16:14:32 -0600

    Add files via upload

---
+++ examen_2da_unidad_4.py
@@ -0,0 +1,15 @@
+print(" ")
+print("nolasco_aguilar_martha_sofia_0949_3w")
+print(" ")
+#inicia una lista 
+thisdict ={
+    "nombre":"sofia",
+    "edad":16,
+    "direccion":"joaquin torres garcia #2124",
+    "telefono":6561315577,
+}
+#imprime los datos de la lista 
+print("nombre",thisdict["nombre"])
+print("tiene",thisdict["edad"])
+print("vive en ",thisdict["direccion"])
+print("su numero de telefono es ",thisdict["telefono"])
