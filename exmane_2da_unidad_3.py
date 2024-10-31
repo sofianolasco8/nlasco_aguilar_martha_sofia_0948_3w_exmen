2d9e46c8327fc7d954fdd187631d58139199c263
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-31 16:02:59 -0600

    Add files via upload

---
+++ exmane_2da_unidad_3.py
@@ -0,0 +1,17 @@
+print(" ")
+print("nolasco_aguilar_martha_sofia_0949_3w")
+print(" ")
+calif =["matematicas", "fisica", "quimica", "historia", "lengua"] #crea la lista de materias
+calif[0]= int(input("ingresa tu calificacion ")) #indica al usuario ingresar su calificacion de esta materia
+calif[1]= int(input("ingresa tu calificacion "))#indica al usuario ingresar su calificacion de esta materia
+calif[2]= int(input("ingresa tu calificacion "))#indica al usuario ingresar su calificacion de esta materia
+calif[3]= int(input("ingresa tu calificacion "))#indica al usuario ingresar su calificacion de esta materia
+calif[4]= int(input("ingresa tu calificacion "))#indica al usuario ingresar su calificacion de esta materia
+
+print("En matematicas sacaste",calif[0]) #imprime la materia junto con su calificacion
+print("En fisica sacaste",calif[1])#imprime la materia junto con su calificacion
+print("En quimica sacaste ",calif[2])#imprime la materia junto con su calificacion
+print("En historia sacaste ",calif[3])#imprime la materia junto con su calificacion
+print("En lengua sacaste ",calif[4])#imprime la materia junto con su calificacion
+
+
