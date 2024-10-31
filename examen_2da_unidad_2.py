30ea6820201a7a4c3453807683f4a31be223eb6c
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-31 15:48:30 -0600

    Add files via upload

---
+++ examen_2da_unidad_2.py
@@ -0,0 +1,16 @@
+print(" ")
+print("nolasco_aguilar_martha_sofia_0948_3w")
+print(" ")
+thisdict={ #se abre una lista 
+    "matematicas":6, #define un valor de la lista 
+    "Física": 4,#define un valor de la lista 
+    "Química": 5,#define un valor de la lista 
+
+}
+print("matematicas:",thisdict["matematicas"])#indica que se va a imprimir el nombre de la materia y loos creditos
+print("fisica:",thisdict["Física"])#indica que se va a imprimir el nombre de la materia y loos creditos
+print("quimica:",thisdict["Química"])#indica que se va a imprimir el nombre de la materia y loos creditos
+
+print(thisdict["matematicas"])#indica que solo se va a imprimir los creditos 
+print(thisdict["Física"])#indica que solo se va a imprimir los creditos 
+print(thisdict["Química"])#indica que solo se va a imprimir los creditos 
